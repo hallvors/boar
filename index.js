706e37d13d2b8d6a846c47dcb81d0593c2c6e4b3
Dainis <dainis.tillers@gmail.com>
2015-10-15 18:03:31 +0300

    Move summoner to boar and add script in bin folder for starting tab with selected engine(#9)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 
+let paths = require('./lib/paths');
+
 module.exports = {
-	tabPath  : require('path').resolve(__dirname, 'tab.js'),
-	boarPath : __dirname
+	tabPath  : paths.tabPath,
+	boarPath : paths.boarPath,
+	Summoner : require('./lib/summoner')
 };
