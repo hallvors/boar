7363e5f388cd78c563736d6b07a5a4ba621d70a7
Seif Lotfy <seif.lotfy@gmail.com>
2015-12-06 18:34:42 +0100

    Fix #12
    Why does taking a screenshot resize the viewport?

--- boar/tab.js
+++ boar/tab.js
@@ -263,20 +263,8 @@ Tab.prototype._getScreenshot = function (callback) {
   var self = this;
   self._busy = true;
   utils.fixFlash();
-  var origSize = {
-    width: self._page.viewportSize.width,
-    height: self._page.viewportSize.height
-  };
-  self._page.viewportSize = {
-    width: 1024,
-    height: 4096
-  };
   window.setTimeout(function () {
     self._waitForResources(60000, function () {
-      self._page.viewportSize = {
-        width: origSize.width,
-        height: origSize.height
-      };
       var base64 = self._page.renderBase64('PNG');
       self._busy = false;
       callback({
