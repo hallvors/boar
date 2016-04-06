2aed6f13e9714018ae87fc8ecc6d861ab413c92c
Hallvord Reiar M. Steen <hsteen@mozilla.com>
2016-04-06 11:49:19 +0200

    Improve page width plugin, fixes #69

--- boar/plugins/page-width-check.js
+++ boar/plugins/page-width-check.js
@@ -15,12 +15,19 @@ pageWidthCheck.prototype.init = function (page) {
 };
 
 pageWidthCheck.prototype.onLoadFinished = function () {
-	var self = this;
-    self.res = self._page.evaluate(function(){
-	    var docwidth = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth);
-	    if(docwidth > (window.innerWidth * 1.02))return 'too wide'; // page is more than 2 percentage points wider than than window width
-	    return 'fits screen';
-    });
+  var self = this;
+  self.res = self._page.evaluate(function(){
+    var docwidth = 0;
+    if(document.body) { // For unknown reasons, doc.body is not always there/ready when this runs
+      docwidth = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth);
+    } else {
+      docwidth = document.documentElement.scrollWidth;
+    }
+    if(docwidth > (window.innerWidth * 1.01)) {
+      return 'too wide'; // page is more than 1 percentage points wider than than window width
+    }
+    return 'fits screen';
+  });
 };
 
 pageWidthCheck.prototype.getResult = function () {
